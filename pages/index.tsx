d834f018996111884b8eb909f9b108821b4ae638
FranKydeSU <franapasin.s@gmail.com>
2022-09-28 02:22:19 +0700

    test webhook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import type { NextPage } from 'next'
-import Navbar from '../components/navbar'
+import type { NextPage } from "next";
+import Navbar from "../components/navbar";
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-wrap bg-gray-100 w-full h-screen">
-      <Navbar/>
+      <Navbar />
+      <div className="w-9/12">
+        <div className="p-4 text-gray-500">Content here...</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
