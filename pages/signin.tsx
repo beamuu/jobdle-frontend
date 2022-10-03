f6d3bef380e41e8914cdc451c74d22bcbd500d94
FranKydeSU <franapasin.s@gmail.com>
2022-10-04 05:12:36 +0700

    demo SignIn&SignUp Page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -22,34 +22,54 @@ function SignIn() {
     console.log(res.data);
   };
   return (
-    <>
-      <div>Sign in</div>
-      <form onSubmit={handleSignin}>
-        <label>Email</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setUsername(e.target.value)
-          }
-        />
-        <br />
-        <label>Password</label>
-        <input
-          type="text"
-          value={password}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        />
-        <button
-          type="submit"
-          className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium"
-        >
-          Sign In
-        </button>
-      </form>
-    </>
+    <div className="bg-blue-200 h-screen w-screen grid justify-items-center">
+      <div className="bg-white sm:w-8/12 md:w-8/12 lg:w-4/12 my-20 p-10 rounded-xl border border-transparent">
+        <p className="font-bold text-3xl text-center my-5">Login</p>
+        <form onSubmit={handleSignin}>
+          <div className="mb-3">
+            <label className="block font-medium text-gray-700 my-1">
+              Email
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="text"
+              value={username}
+              placeholder="Type your email"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setUsername(e.target.value)
+              }
+            />
+          </div>
+          <div className="mb-3">
+            <label className="block font-medium text-gray-700 my-1">
+              Password
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="text"
+              value={password}
+              placeholder="Type your password"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+            />
+          </div>
+          <div className="flex justify-end mb-3">
+            <p className="text-blue-500 hover:underline hover:pointer">
+              Forgot Password?
+            </p>
+          </div>
+          <div className="grid mb-3">
+            <button
+              type="submit"
+              className="transition rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-lg shadow-blue-500/50 hover:bg-blue-400"
+            >
+              LOGIN
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,23 +1,96 @@
+import { ChangeEvent, useState } from "react";
+
 function SignUp() {
-    return (
-    <>
-      <div>Sign Up</div>
-      <form>
-        <label>First-Name</label>
-        <input type="text" />
-        <label>Last-Name</label>
-        <input type="text" />
-        <label>Email</label>
-        <input type="email" />
-        <br />
-        <label>Password</label>
-        <input type="password" />
-        <label>Confirm Password</label>
-        <input type="password" />
-        <button type="submit" className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium">Sign Up</button>
-      </form>
-    </>
-    );
-  }
-  
-  export default SignUp;
+  const [email, setEmail] = useState("");
+
+  return (
+    <div className="bg-blue-200 h-screen w-screen grid justify-items-center">
+      <div className="bg-white w-9/12 my-20 p-10 rounded-xl border border-transparent">
+        <p className="font-bold text-3xl text-center my-5">Sign Up</p>
+        <form>
+          <div className="mb-3 lg:flex">
+            <div className="lg:flex-1 lg:mr-3">
+              <label className="block font-medium text-gray-700 my-1">
+                First Name
+              </label>
+              <input
+                className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                type="text"
+                value=""
+                placeholder=""
+              />
+            </div>
+            <div className="lg:flex-1">
+              <label className="block font-medium text-gray-700 my-1">
+                Surname
+              </label>
+              <input
+                className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                type="text"
+                value=""
+                placeholder=""
+              />
+            </div>
+          </div>
+          <div className="mb-3">
+            <label className="block font-medium text-gray-700 my-1">
+              Email address
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="text"
+              value={email}
+              onChange={(e :ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              placeholder="Your email"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="block font-medium text-gray-700 my-1">
+              Username
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="text"
+              value=""
+              placeholder="Your email"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="block font-medium text-gray-700 my-1">
+              Password
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="text"
+              value=""
+              placeholder="Password"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="block font-medium text-gray-700 my-1">
+              Confirm Password
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              type="text"
+              value=""
+              placeholder="Confirm Password"
+            />
+          </div>
+          <div className="grid mb-3">
+            <button
+              type="submit"
+              className="transition rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-lg shadow-blue-500/50 hover:bg-blue-400"
+            >
+              SIGN UP
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
